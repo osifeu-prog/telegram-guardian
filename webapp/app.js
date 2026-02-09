2cd790bbc60e9c89faba3d461c4b396b01bc8c43
osifeu-prog <you@example.com>
2026-02-09 14:29:49 +0200

    Added Mini-App (WebApp) structure and /app command

---
+++ webapp/app.js
@@ -0,0 +1,18 @@
+﻿const tg = window.Telegram.WebApp;
+tg.expand();
+
+function getBalance() {
+  tg.sendData(JSON.stringify({ action: "balance" }));
+}
+
+function openRequests() {
+  tg.sendData(JSON.stringify({ action: "requests" }));
+}
+
+function openMarketplace() {
+  tg.sendData(JSON.stringify({ action: "marketplace" }));
+}
+
+function openP2P() {
+  tg.sendData(JSON.stringify({ action: "p2p" }));
+}
